5ce10fd09477b5fcf829d78d3f9fb68080027594
Cyberscrewed <142054999+Cyberscrewed@users.noreply.github.com>
2024-04-15 18:27:58 -0500

    Update television.py

--- television.py
+++ television.py
@@ -4,26 +4,20 @@ class Television:
     MIN_CHANNEL: int = 0
     MAX_CHANNEL: int = 3
 
-    def __init__(self) -> None:
-        self.__status: bool = False
-        self.__muted: bool = False
-        self.__volume: int = self.MIN_VOLUME
-        self.__channel: int = self.MIN_CHANNEL
-        self.__old_volume: int = 0
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
+        self.__old_volume = 0
 
-    def power(self) -> None:
-        """
-        Toggles power status of TV
-        """
+    def power(self):
         if self.__status:
             self.__status = False
         else:
             self.__status = True
 
-    def mute(self) -> None:
-        """
-        Toggles mute status of TV
-        """
+    def mute(self):
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -33,30 +27,21 @@ class Television:
                 self.__old_volume = self.__volume
                 self.__volume = self.MIN_VOLUME
 
-    def channel_up(self) -> None:
-        """
-        Increases channel, looping if at max
-        """
+    def channel_up(self):
         if self.__status:
             if self.__channel < self.MAX_CHANNEL:
                 self.__channel += 1
             else:
                 self.__channel = self.MIN_CHANNEL
 
-    def channel_down(self) -> None:
-        """
-        Decreases channel, looping if at min
-        """
+    def channel_down(self):
         if self.__status:
             if self.__channel > self.MIN_CHANNEL:
                 self.__channel -= 1
             else:
                 self.__channel = self.MAX_CHANNEL
 
-    def volume_up(self) -> None:
-        """
-        Increases volume, halting at max
-        """
+    def volume_up(self):
         if self.__status:
             if self.__muted:
                 self.mute()
@@ -66,10 +51,7 @@ class Television:
                 if self.__volume < self.MAX_VOLUME:
                     self.__volume += 1
 
-    def volume_down(self) -> None:
-        """
-        Decreases volume, halting at min
-        """
+    def volume_down(self):
         if self.__status:
             if self.__muted:
                 self.mute()
@@ -79,5 +61,5 @@ class Television:
                 if self.__volume > self.MIN_VOLUME:
                     self.__volume -= 1
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
